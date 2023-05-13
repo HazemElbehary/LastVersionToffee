5c2a4c2b515c132891f6e3125f75c80a8e5a2a79
Mr_Code <93862014+MR1WOLF@users.noreply.github.com>
2023-05-13 22:05:28 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -15,7 +15,50 @@ public class Customer {
         password=pass;
         phoneNumber = phNum;
     }
-    
+    public static void OTP(String PhoneNumber){
+        String recipientPhoneNumber = PhoneNumber; // Replace with recipient's phone number
+        String otp = generateOTP(6);
+        sendOTPSMS(recipientPhoneNumber, otp);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Your Verification Code:");
+        String OTP = in.nextLine();
+        ValidateOTP(otp,OTP);
+    }
+    public static String generateOTP(int length) {
+        String numbers = "0123456789";
+        Random rndm_method = new Random();
+        char[] otp = new char[length];
+        for (int i = 0; i < length; i++) {
+            otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+        }
+
+        return new String(otp);
+    }
+    public static void ValidateOTP(String otp,String userIn) {
+//        System.out.println(otp);
+//        System.out.println(userIn);
+        if(Objects.equals(otp, userIn)){
+            System.out.printf("Perfect You are now Validated !");
+        }
+        else {
+            System.out.printf("Invalid Code !");
+        }
+    }
+    public static void sendOTPSMS(String recipientPhoneNumber, String otp) {
+        String accountSid = "ACb219480261d6f99ad399ed06a457f9f6"; // Replace with your Twilio account SID
+        String authToken = "162f976cf040df390951c44c3d220c16"; // Replace with your Twilio auth token
+        String twilioPhoneNumber = "+12705182337"; // Replace with your Twilio phone number
+
+        Twilio.init(accountSid, authToken);
+
+        Message message = Message.creator(
+                        new PhoneNumber(recipientPhoneNumber),
+                        new PhoneNumber(twilioPhoneNumber),
+                        "Your OTP: " + otp)
+                .create();
+
+        System.out.println("OTP sent successfully to " + recipientPhoneNumber);
+    }
     
     void regester(){
         ArrayList<Object> dataOneUser = new ArrayList<>();
@@ -56,6 +99,7 @@ public class Customer {
             return;
         }
         dataOneUser.add(phoneNumber);
+        OTP(phoneNumber);
         data.add(dataOneUser);
     }
     void login(){
@@ -92,4 +136,6 @@ public class Customer {
      ArrayList<ArrayList<Object>> getCart(){
          return cart;
     }
+   
+    
 }
